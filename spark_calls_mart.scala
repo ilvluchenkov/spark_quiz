3e58925e5b273964cb186e58ac26ae64daccaf2c
ILYA <48024021+ilvluchenkov@users.noreply.github.com>
2020-11-08 12:24:44 +0300

    Update spark_calls_mart.scala
    com2

--- spark_calls_mart.scala
+++ spark_calls_mart.scala
@@ -1,9 +1,11 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{avg, sum, max, min}
+//import org.apache.spark.sql.functions.{avg, sum, max, min}
+import org.apache.spark.sql.functions._
 
 object spark_calls_mart extends App {
 
-  val spark = SparkSession.builder
+  val spark = SparkSession
+    .builder
     .appName( name = "spark_calls_mart")
     .enableHiveSupport()
     .getOrCreate()
@@ -23,7 +25,7 @@ object spark_calls_mart extends App {
     .mode("append")
     .option("path", "hdfs://ns-etl/warehouse/tablespace/external/hive/etl_stg.db/call_stats_iv")
     .format("parquet")
-    .saveAsTable("etl_stg.call_stats_olch")
+    .saveAsTable("etl_stg.call_stats_iv")
 
   val datamartDf = spark
     .table("etl_stg.call_stats_iv")
@@ -31,17 +33,23 @@ object spark_calls_mart extends App {
 
   datamartDf.groupBy("state")
     .agg(
-      min("total_day_calls").as("min_calls"),
-      max("total_day_calls").as("max_calls"),
-      avg("total_day_calls").as("avg_calls"),
-      sum("total_day_minutes").as("sum_minutes"))
-    .coalesce(1)
-    .write
-    .mode("append")
-    .option("path", "hdfs://ns-etl/warehouse/tablespace/external/hive/etl_stg.db/call_stats_mart_iv")
-    .format("parquet")
-    .saveAsTable("etl_stg.call_stats_mart_iv")
+      min("total_day_calls").as("min_total_day_calls"),
+		  max("total_day_calls").as("max_total_day_calls"),
+		  avg("total_day_calls").as("avg_total_day_calls"),
+		  sum("total_day_minutes").as("sum_total_day_calls_minutes"))
+		  .show(false)
+      .coalesce(1)
+      .write
+      .mode("append")
+      .option("path", "hdfs://ns-etl/warehouse/tablespace/external/hive/etl_stg.db/call_stats_mart_iv")
+      .format("parquet")
+  .saveAsTable("etl_stg.call_stats_mart_iv")
 
 }
+
+
+ 
+
+
 
 
