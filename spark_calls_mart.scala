e716f3d29c71e68d6ac5f265bc66c3ceefa03716
ILYA <48024021+ilvluchenkov@users.noreply.github.com>
2020-11-08 11:33:22 +0300

    Add files via upload

---
+++ spark_calls_mart.scala
@@ -0,0 +1,47 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{avg, sum, max, min}
+
+object spark_calls_mart extends App {
+
+  val spark = SparkSession.builder
+    .appName( name = "spark_calls_mart")
+    .enableHiveSupport()
+    .getOrCreate()
+
+  val sourceTable = spark.read
+    .format("jdbc")
+    .option("url", "jdbc:postgresql://10.31.153.191:30134/demo_base")
+    .option("dbtable", "demo_base.public.call_stats")
+    .option("user", "spark_demo")
+    .option("password", "jw8s0F4")
+    .option("driver", "org.postgresql.Driver")
+    .load()
+
+  sourceTable
+    .coalesce(1)
+    .write
+    .mode("append")
+    .option("path", "hdfs://ns-etl/warehouse/tablespace/external/hive/etl_stg.db/call_stats_olch")
+    .format("parquet")
+    .saveAsTable("etl_stg.call_stats_olch")
+
+  val datamartDf = spark
+    .table("etl_stg.call_stats_olch")
+    .select("state", "total_day_calls", "total_day_minutes")
+
+  datamartDf.groupBy("state")
+    .agg(
+      min("total_day_calls").as("min_calls"),
+      max("total_day_calls").as("max_calls"),
+      avg("total_day_calls").as("avg_calls"),
+      sum("total_day_minutes").as("sum_minutes"))
+    .coalesce(1)
+    .write
+    .mode("append")
+    .option("path", "hdfs://ns-etl/warehouse/tablespace/external/hive/etl_stg.db/call_stats_mart_olch")
+    .format("parquet")
+    .saveAsTable("etl_stg.call_stats_mart_olch")
+
+}
+
+
